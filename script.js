e5cbdfc0a8b9b46dcc549a8b479175375c909e80
Akshar-u-2077 <aksharu73@gmail.com>
2026-02-24 13:40:12 +0530

    redisgned the website using css elements

--- script.js
+++ script.js
@@ -1,2 +1,11 @@
-console.log(0 == false);
-console.log(0 === false);
+let num = [5, -2, 10, -3];
+let maxnum=num[0];
+for(let i=0;i<num.length;i++)
+{
+    if(maxnum<num[i])
+    {
+        maxnum=num[i];
+    }
+
+}
+console.log(maxnum);
